30c9f1dbd334e9a04ddcae4f5385fdb86453a161
anirudha.panda <anirudha.panda@AnirudhaPanda.covalensedigital.net>
2021-11-17 11:42:50 +0530

    Commit_01-: Initial commit

---
+++ HibernateDemo2/src/com/covalense/app/App_01.java
@@ -0,0 +1,34 @@
+package com.covalense.app;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.covalense.beans.Employee;
+
+public class App_01 {
+
+	public static void main(String[] args) {
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		org.hibernate.metamodel.Metadata meta = new org.hibernate.metamodel.MetadataSources(ssr).getMetadataBuilder()
+				.build();
+		SessionFactory sessionFactory = meta.getSessionFactoryBuilder().build();
+		Session session = sessionFactory.openSession();
+		Transaction t = session.beginTransaction();
+
+		Employee emp = new Employee();
+		emp.setId(11);
+		emp.setFirstName("priyanka");
+		emp.setLastName("warrior");
+
+		session.save(emp);
+		t.commit();
+		session.close();
+		sessionFactory.close();
+
+	}
+
+}
